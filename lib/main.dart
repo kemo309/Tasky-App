0b4a83a9badbd38e59652ab479f1bbf3f26b6ff6
kemo309 <mohamedkemo1666@gmail.com>
2026-02-04 10:49:14 +0200

    feat: Implement authentication screens and integrate Firebase Auth
    - Added LoginScreen for user login functionality with email and password.
    - Created RegisterScreen for user registration with username, email, and password.
    - Introduced SplashScreen for initial app loading with animations.
    - Added Onboarding screen placeholder for future enhancements.
    - Implemented TextFormFieldWidget for reusable form fields across screens.
    - Integrated animate_do package for splash screen animations.
    - Updated pubspec.yaml and pubspec.lock to include new dependencies.
    - Added new assets for icons used in the splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tasky_app/features/auth/screens/register_screen.dart';
+import 'package:tasky_app/features/auth/screens/splash_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:tasky_app/features/auth/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(TaskApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TaskApp extends StatelessWidget {
+  const TaskApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Splash Screen and App Icon")),
-        body: Center(child: Text("Home Screen")),
-      ),
+      home: SplashScreen(),
+      routes: {
+        LoginScreen.routeName: (context) => LoginScreen(),
+        RegisterScreen.routeName: (context) => RegisterScreen(),
+        SplashScreen.routeName: (context) => SplashScreen(),
+      },
     );
   }
 }
-
-
-
-
-// 1- add package flutter_native_splash in pubspec.yaml part of dependencies
-
-// 2- design splash android and ios screens
-//    download splash images (icon) in assets folder say splash_ios_android_11.png
-// 3- design splash android 12 screen
-//    # in figma create frame w:640 h:640 and r:320 and center the icon in this frame
-//    # create new frame w:960 h:960 and center the last frame in this frame
-//    # final export the frame as png and name it splash_ios_android_12.png
-// 4- create file in rote app flutter_native_splash.yaml
-//    # writhe in this code in this file 
-      // flutter_native_splash:
-      //   color: "#5F33E1"
-      //   image: assets/icons/splash_ios_android_11.png
-      //   android_12:
-      //     image: assets/icons/splash_android_12.png
-      //     color: "#5F33E1"
-// 5- run => dart run flutter_native_splash:create --path=flutter_native_splash.yaml
-
-
-// 1- add package flutter_launcher_icons in pubspec.yaml dev_dependencies
-// icons app logo
-// 2- app IOS:
-// # add image icon in assets say icon-launcher.png
-// 3- app Android:
-// # go to web page romannurik.github.io
-// # download icon app
-// # add icon in assets say play-store-icon-launcher.png
-
-// write in pubspec.yaml part of dev_dependencies
-// flutter_icons:
-//   android: true
-//   ios: true
-//   image_path: "assets/icons/icon-launcher.png"
-//   adaptive_icon_background: "#5F33E1"
-//   adaptive_icon_foreground: "assets/icons/play-store-icon-launcher.png" 
-
-// final run this
-// flutter pub get
-// flutter pub run flutter_launcher_icons
